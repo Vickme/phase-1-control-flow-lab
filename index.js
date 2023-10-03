d3c5d699245774ecd3cbfdef3c019af1abfed26f
victor <victor.njutha@student.moringaschool.com>
2023-10-02 21:18:18 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
+if(feet === 400) {
+  return "this Is For Me"
+} else{
+  return "this Is Not For Me"
+}
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
+  destination === "diani"? "welcome" : "change Destination"
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "tip">= 200:
+      return "thank You Very Much"
+      case "tip"<200:
+        return "thanks"
+  }
   // Write your code here!
 }
